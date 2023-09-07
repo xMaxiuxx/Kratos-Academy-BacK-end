8fb49bbe51785e363cde5d658db347d005f0ab46
xMaxiuxx <maxik4po@gmail.com>
2023-09-07 14:26:41 -0300

    first commit

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const express = require("express");
 const app = express();
 
 app.listen(3001,()=>{
-    console.log("Funcionando en el Puerto 3001")
+    console.log("Funcionando en el Puerto 3001") 
 })
